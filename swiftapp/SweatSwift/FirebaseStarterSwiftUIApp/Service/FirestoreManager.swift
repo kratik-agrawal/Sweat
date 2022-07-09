7d94c40e8867693676b90657a31923629ff704bc
Pranit Agrawal <pranitagrawal@MacBook-Pro-2.local>
2022-07-08 17:21:59 -0700

    added profile page features

---
+++ swiftapp/SweatSwift/FirebaseStarterSwiftUIApp/Service/FirestoreManager.swift
@@ -0,0 +1,27 @@
+////
+////  FirestoreManager.swift
+////  FirebaseStarterSwiftUIApp
+////
+////  Created by Pranit Agrawal on 7/8/22.
+////  Copyright © 2022 iOS App Templates. All rights reserved.
+////
+//
+//import Foundation
+//import Firebase
+//import FirebaseFirestore
+//
+//class FirestoreManager: ObservableObject {
+//
+//    @Published var name: String = ""
+//
+//    func fetchName() {
+//
+//        let db = Firestore.firestore()
+//
+//        let docRef = db.collection("Users").document("6ZkENUQtFEz2NiTVDJ8r")
+//
+//        doc
+//
+//    }
+//
+//}
--- swiftapp/SweatSwift/FirebaseStarterSwiftUIApp/View/ProfileView.swift
+++ swiftapp/SweatSwift/FirebaseStarterSwiftUIApp/View/ProfileView.swift
@@ -1,31 +1,104 @@
-//
-//  ProfileView.swift
-//  FirebaseStarterSwiftUIApp
-//
-//  Created by Pranit Agrawal on 6/29/22.
-//  Copyright © 2022 iOS App Templates. All rights reserved.
-//
-
 import SwiftUI
 
 struct ProfileView: View {
     
+    //@ObservedObject var model = SignUpViewModel(authAPI: AuthAPI as! AuthAPI, state: AppState)
+
+
     
+    /*init() {
+        model.getData()
+    }
+    */
     
     var body: some View {
-        NavigationView {
-            ZStack{
-                Color.orange
-                    //.edgesIgnoringSafeArea(.top)
+        
+        /*List (model.list, id: \.self) { item in
+            Text(item.name)
+        } */
+            
+        
+        VStack{
+            //Spacer().frame(height: -100)
+            //Color.white
+            //padding(-100)
+            VStack{
+                Image("pfpholder")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .padding(-260)
+                    .offset(x: 155, y: 100)
+                Text("Full Name")
+                    .font(.title)
+                    .bold()
+                    .padding(-70)
+                    .offset(x: 3, y: 70)
+                   // .background(.orange)
+                    .foregroundColor(.orange)
+            }
+            VStack(alignment: .leading, spacing: 12) {
+                HStack {
+                    Image(systemName: "envelope")
+                        .offset(x: -10, y: 42)
+                    Text("sweatuser@gmail.com")
+                        .offset(x: -10, y: 42)
+                }
+            }
+            Divider( ).offset(y: 65).frame( width: 350 )
+            
+            VStack {
+                Text("Your Current Balance")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                    .offset(y: 75)
+                    .foregroundColor(.orange)
+                
+                Text("$12.34")
+                    .bold()
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 310, height: 120)
+                    .background(Rectangle().fill(Color.gray).shadow(radius: 3))
+                    .foregroundColor(.red)
+                    .offset(y: 75)
+                    .font(.largeTitle)
+                   
+                            
+                Text("You Have Made")
+                    .font(.title)
+                    .bold()
+                    .padding()
+                   // .offset(y: 100)
+                    .foregroundColor(.orange)
+                    .offset(y: 75)
+                
+                Text("$12.34")
+                    .bold()
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 310, height: 120)
+                    .background(Rectangle().fill(Color.gray).shadow(radius: 3))
+                    .foregroundColor(.green)
+                    .offset(y: 75)
+                    .font(.largeTitle)
+                    
+                    
+                }
                 
             }
-            .navigationTitle("Profile")
-            .navigationBarTitleDisplayMode(.inline)
+            
+            
         }
+        //   .navigationTitle("Profile")
+         //  .navigationBarTitleDisplayMode(.inline)
     }
     
     
-}
+
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
